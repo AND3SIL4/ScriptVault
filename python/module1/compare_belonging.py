ffa1fc585edd1d09c476e961e352574050973be6
AND3SIL4 <afsilva6236@misena.edu.co>
2024-10-07 17:38:22 -0500

    update

--- python/module1/compare_belonging.py
+++ python/module1/compare_belonging.py
@@ -1,5 +1,6 @@
-import pandas as pd #type: ignore
+import pandas as pd  # type: ignore
 import os
+
 
 def main(params: dict):
     try:
@@ -7,7 +8,7 @@ def main(params: dict):
         file_path: str = params.get("file_path")
         sheet_name: str = params.get("sheet_name")
         col_idx1: int = int(params.get("col_idx1"))
-        col_idx2: int  = int(params.get("col_idx2"))
+        col_idx2: int = int(params.get("col_idx2"))
         inconsistencies_file: str = params.get("in_file")
         new_sheet: str = params.get("new_sheet")
         need_iaxis: bool = bool(params.get("need_iaxis"))
@@ -19,29 +20,35 @@ def main(params: dict):
         validated_values: dict = {}
 
         ##Validate if all the required inputs are present
-        if not all([
-            file_path, 
-            sheet_name,
-            new_sheet,
-            inconsistencies_file,
-            list_file,
-            sheet_name_list
-        ]):
+        if not all(
+            [
+                file_path,
+                sheet_name,
+                new_sheet,
+                inconsistencies_file,
+                list_file,
+                sheet_name_list,
+            ]
+        ):
             return "Error: an input param is missing"
 
         ##Read the book and load it
-        df: pd.DataFrame = pd.read_excel(file_path, sheet_name=sheet_name, engine="openpyxl")
-        list_df: pd.DataFrame = pd.read_excel(list_file, sheet_name=sheet_name_list, engine="openpyxl")
+        df: pd.DataFrame = pd.read_excel(
+            file_path, sheet_name=sheet_name, engine="openpyxl"
+        )
+        list_df: pd.DataFrame = pd.read_excel(
+            list_file, sheet_name=sheet_name_list, engine="openpyxl"
+        )
 
         ##Apply validation
         df["is_valid"] = df.apply(
             lambda row: validate(
-                str(row.iloc[col_idx1]), ##Key
-                str(row.iloc[col_idx2]), ##Value
-                validated_values, ##Dictionary as a global variable
-                list_df.iloc[:, except_idx].dropna().astype(str).values.tolist()
-            ), 
-            axis=1
+                str(row.iloc[col_idx1]),  ##Key
+                str(row.iloc[col_idx2]),  ##Value
+                validated_values,  ##Dictionary as a global variable
+                list_df.iloc[:, except_idx].dropna().astype(str).values.tolist(),
+            ),
+            axis=1,
         ).astype(bool)
 
         ##Make a column to know if is "TRUE" or "FALSE"
@@ -51,14 +58,18 @@ def main(params: dict):
         if not need_iaxis:
             if not filtered_file.empty:
                 ##Get the coordinates
-                filtered_file["COORDINATE_1"]  = filtered_file.apply(
-                    lambda row: f"{get_excel_column_name(col_idx1 + 1)}{row.name + 2}", axis=1
+                filtered_file["COORDINATE_1"] = filtered_file.apply(
+                    lambda row: f"{get_excel_column_name(col_idx1 + 1)}{row.name + 2}",
+                    axis=1,
                 )
                 filtered_file["COORDINATE_2"] = filtered_file.apply(
-                    lambda row: f"{get_excel_column_name(col_idx2 + 1)}{row.name + 2}", axis=1
+                    lambda row: f"{get_excel_column_name(col_idx2 + 1)}{row.name + 2}",
+                    axis=1,
                 )
                 ##Store the inconsistencies into the inconsistencies file
-                return append_inconsistencias(inconsistencies_file, new_sheet, filtered_file)                
+                return append_inconsistencias(
+                    inconsistencies_file, new_sheet, filtered_file
+                )
             else:
                 return "Validacion realizada, no se encontraron inconsistencias"
         else:
@@ -68,10 +79,12 @@ def main(params: dict):
             ##Get the coordinates
             if not new_df.empty:
                 new_df["COORDINATE_1"] = new_df.apply(
-                    lambda row: f"{get_excel_column_name(col_idx1 + 1)}{row.name + 2}", axis=1
+                    lambda row: f"{get_excel_column_name(col_idx1 + 1)}{row.name + 2}",
+                    axis=1,
                 )
                 new_df["COORDINATE_2"] = new_df.apply(
-                    lambda row: f"{get_excel_column_name(col_idx2 + 1)}{row.name + 2}", axis=1
+                    lambda row: f"{get_excel_column_name(col_idx2 + 1)}{row.name + 2}",
+                    axis=1,
                 )
                 ##Store the inconsistencies into the inconsistencies file
                 append_inconsistencias(inconsistencies_file, new_sheet, new_df)
@@ -81,15 +94,19 @@ def main(params: dict):
     except Exception as e:
         return f"Error: {e}"
 
+
 def get_excel_column_name(n):
     """Convert a column number (1-based) to Excel column name (e.g., 1 -> A, 28 -> AB)."""
-    result = ''
+    result = ""
     while n > 0:
-        n, remainder = divmod(n-1, 26)
+        n, remainder = divmod(n - 1, 26)
         result = chr(65 + remainder) + result
     return result
 
-def validate(key: str, value: str, validated: dict[str], exception_list: list[str]) -> bool:
+
+def validate(
+    key: str, value: str, validated: dict[str], exception_list: list[str]
+) -> bool:
     ##Set the local variables
     if key in validated:
         if validated[key] == value or key in exception_list:
@@ -110,8 +127,9 @@ def append_inconsistencias(file_path: str, new_sheet: str, data_frame) -> None:
         with pd.ExcelWriter(
             file_path, engine="openpyxl", if_sheet_exists="replace", mode="a"
         ) as writer:
-            data_frame.to_excel(writer, index=False, sheet_name=new_sheet)        
+            data_frame.to_excel(writer, index=False, sheet_name=new_sheet)
             return "Inconsistencias registradas correctamente"
+
 
 if __name__ == "__main__":
     params = {
@@ -124,7 +142,7 @@ if __name__ == "__main__":
         "need_iaxis": True,
         "list_file": r"C:\ProgramData\AutomationAnywhere\Bots\Logs\AD_RCSN_SabanaPagosYBasesParaSinestralidad\InputFolder\Listas - BOT.xlsx",
         "except_idx": "2",
-        "sheet_name_list": "EXCEPCIONES COPARACION SINIES"
+        "sheet_name_list": "EXCEPCIONES COPARACION SINIES",
     }
 
     print(main(params))
--- python/module1/cross_latest_file.py
+++ python/module1/cross_latest_file.py
@@ -12,6 +12,7 @@ def main(params: dict) -> None:
         latest_file: str = params.get("latest_file")
         sheet_latest_name: str = params.get("sheet_latest_name")
         col_idx: int = int(params.get("col_idx"))
+        cut_date: str = params.get("cut_date")
 
         ##Validate if all the required files are present
         if not all(
@@ -26,8 +27,7 @@ def main(params: dict) -> None:
             return "ERROR: an input required param is missing"
 
         ##Set the dates for filtering the files
-        cut_off_date = pd.to_datetime(datetime.now().date(), format="%d/%m/%Y")
-        cut_off_date = cut_off_date - pd.DateOffset(months=1)
+        cut_off_date = pd.to_datetime(cut_date, format="%d/%m/%Y")
         year = datetime.today().year
         date = f"01/01/{year}"
         initial_date = pd.to_datetime(date, format="%d/%m/%Y")
@@ -54,10 +54,75 @@ def main(params: dict) -> None:
         reserva_latest_cut = file_filtered.iloc[:, 34].sum()
         reserva_current_cut = latest_filtered.iloc[:, 34].sum()
 
+        print(reserva_latest_cut)
+        print(reserva_current_cut)
+
         # Validate if the values of "Reserva" are the same
-        return reserva_latest_cut == reserva_current_cut
+        if reserva_latest_cut != reserva_current_cut:
+            file_filtered["LLAVE_CRUCE"] = (
+                file_filtered.iloc[:, 0].astype(str)
+                + "-"
+                + file_filtered.iloc[:, 2].astype(str)
+                + "-"
+                + file_filtered.iloc[:, 32].astype(str)
+            )
+            file_filtered["RESERVA_CRUCE"] = file_filtered.iloc[:, 34]
+
+            latest_filtered["LLAVE_CRUCE"] = (
+                latest_filtered.iloc[:, 0].astype(str)
+                + "-"
+                + latest_filtered.iloc[:, 2].astype(str)
+                + "-"
+                + latest_filtered.iloc[:, 32].astype(str)
+            )
+
+            ##Make crossover file
+            merge_df: pd.DataFrame = latest_filtered.merge(
+                file_filtered[["LLAVE_CRUCE", "RESERVA_CRUCE"]],
+                on="LLAVE_CRUCE",
+                how="left",
+                suffixes=("_OLD", "_NEW"),
+            )
+
+            merge_df["VALIDATION"] = merge_df.iloc[:, 34] == merge_df.iloc[:, -1]
+            inconsistencies = merge_df[~merge_df["VALIDATION"]].copy()
+
+            inconsistencies["COORDINATE_1"] = inconsistencies.apply(
+                lambda row: f"{get_excel_column_name(35)}{row.name + 2}",
+                axis=1,
+            )
+
+            return append_inconsistencias(
+                inconsistencies_file, "ValidacionTotalReserva", inconsistencies
+            )
+        else:
+            return "Validacion realizada, no se encontraron novedades por registrar"
     except Exception as e:
         return f"ERROR: {e}"
+
+
+def append_inconsistencias(file_path: str, new_sheet: str, data_frame) -> None:
+    """This function get the inconsistencies data frame and append it into the inconsistencies file"""
+    if os.path.exists(file_path):
+        with pd.ExcelFile(file_path, engine="openpyxl") as xls:
+            if new_sheet in xls.sheet_names:
+                existing = pd.read_excel(xls, sheet_name=new_sheet, engine="openpyxl")
+                data_frame = pd.concat([existing, data_frame], ignore_index=True)
+
+        with pd.ExcelWriter(
+            file_path, engine="openpyxl", if_sheet_exists="replace", mode="a"
+        ) as writer:
+            data_frame.to_excel(writer, index=False, sheet_name=new_sheet)
+            return "Inconsistencias registradas correctamente"
+
+
+def get_excel_column_name(n):
+    """Convert a column number (1-based) to Excel column name (e.g., 1 -> A, 28 -> AB)."""
+    result = ""
+    while n > 0:
+        n, remainder = divmod(n - 1, 26)
+        result = chr(65 + remainder) + result
+    return result
 
 
 if __name__ == "__main__":
@@ -65,9 +130,10 @@ if __name__ == "__main__":
         "file_path": r"C:\ProgramData\AutomationAnywhere\Bots\Logs\AD_RCSN_SabanaPagosYBasesParaSinestralidad\TempFolder\BASE DE REPARTO 2024.xlsx",
         "inconsistencias_file": r"C:\ProgramData\AutomationAnywhere\Bots\Logs\AD_RCSN_SabanaPagosYBasesParaSinestralidad\OutputFolder\Inconsistencias\InconBaseReparto.xlsx",
         "sheet_name": "CASOS NUEVOS",
-        "latest_file": r"C:\ProgramData\AutomationAnywhere\Bots\Logs\AD_RCSN_SabanaPagosYBasesParaSinestralidad\OutputFolder\Historico base reparto\BASE DE REPARTO 2024.xlsx",
+        "latest_file": r"C:\ProgramData\AutomationAnywhere\Bots\Logs\AD_RCSN_SabanaPagosYBasesParaSinestralidad\OutputFolder\Historico base reparto\BASE DE REPARTO 2024 ACTUALIZADO.xlsx",
         "sheet_latest_name": "CASOS NUEVOS",
         "col_idx": "24",
+        "cut_date": "30/07/2024",
     }
 
     print(main(params))
