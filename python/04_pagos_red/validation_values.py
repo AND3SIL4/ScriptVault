b5aab758a12168fda8241a7e47db97c57c518444
AND3SIL4 <afsilva6236@misena.edu.co>
2024-12-18 21:39:09 -0500

    update(values validation) updated script

--- python/04_pagos_red/validation_values.py
+++ python/04_pagos_red/validation_values.py
@@ -1,4 +1,5 @@
 import pandas as pd  # type: ignore
+import numpy as np  # type: ignore
 from typing import Optional
 from datetime import datetime
 
@@ -12,6 +13,7 @@ class ValuesValidation:
         sheet_name,
         file_name: str,
         previous_file: str,
+        temp_file: str,
     ):
         self.file_path = file_path
         self.inconsistencies_file = inconsistencies_file
@@ -19,6 +21,7 @@ class ValuesValidation:
         self.sheet_name = sheet_name
         self.file_name = file_name
         self.previous_file = previous_file
+        self.temp_file = temp_file
 
     def read_excel(self, file_path: str, sheet_name: str) -> pd.DataFrame:
         """Method for returning a data frame"""
@@ -86,9 +89,10 @@ class ValuesValidation:
         # Extract the text
         if start > 0 and end > 0:
             file_date = self.file_name[start:end]
+            file_date = file_date.replace("-", "/")
             return file_date
         else:
-            return "error el obtener la fecha"
+            return "Error obteniendo fecha"
 
 
 # Instance the main class
@@ -157,36 +161,59 @@ def cross_file(propuesta_df: pd.DataFrame, acm_df: pd.DataFrame) -> pd.DataFrame
     return merged_df
 
 
-def apply_formulas(data_frame: pd.DataFrame, previous_df: pd.DataFrame) -> None:
+def apply_formulas(data_frame: pd.DataFrame, historical_df: pd.DataFrame) -> None:
     """Method to apply formulas to the merged data frame"""
-    # Apply formulas
+    # Get the list of the historical report
+    historical_radicados: list[str] = (
+        historical_df.iloc[:, 1].dropna().astype(str).to_list()
+    )
+    historical_key: list[str] = historical_df.iloc[:, 3].dropna().astype(str).to_list()
 
-    return previous_df
-    data_frame["Diferencia Aprobado-Liquidado"] = data_frame["Valor Liquidado"].astype(
-        int
-    ) - data_frame["valor aprobado"].astype(int)
-    data_frame["Validacion Valores"] = data_frame.iloc[:, 2].astype(
+    # Convertir columnas a valores numéricos
+    columns_to_convert = ["Valor Liquidado", "valor aprobado", "VR. MOVIMIENTO 100%"]
+    for column in columns_to_convert:
+        data_frame[column] = (
+            data_frame[column]
+            .str.replace(",", "")
+            .str.replace(".", "")
+            .str.replace(" ", "")
+        )
+        data_frame[column] = pd.to_numeric(data_frame[column], errors="coerce").fillna(
+            0
+        )
+
+    # Aplicar fórmulas
+    data_frame[historical_df.columns[6]] = (
+        data_frame["valor aprobado"] - data_frame["Valor Liquidado"]
+    )
+
+    # Validate valores reported
+    data_frame[historical_df.columns[7]] = data_frame.iloc[:, 2].astype(
         int
     ) == data_frame.iloc[:, 5].astype(int)
-    # Validate duplicates
-    data_frame["Duplicados casa matriz"] = (
-        data_frame[data_frame.columns[1]]
-        .duplicated(keep=False)
-        .map({True: 2, False: 1})
-    )
-    data_frame["Duplicados llave"] = (
-        data_frame[data_frame.columns[3]]
-        .duplicated(keep=False)
-        .map({True: 2, False: 1})
-    )
-    data_frame["Formato numero radicado"] = data_frame[data_frame.columns[1]].apply(
+
+    # Validate records duplicates
+    data_frame[historical_df.columns[8]] = (
+        data_frame.iloc[:, 1].duplicated(keep=False)
+        | data_frame.iloc[:, 1].isin(historical_radicados)
+    ).map({True: 2, False: 1})
+
+    data_frame[historical_df.columns[9]] = (
+        data_frame.iloc[:, 3].duplicated(keep=False)
+        | data_frame.iloc[:, 3].isin(historical_key)
+    ).map({True: 2, False: 1})
+
+    data_frame[historical_df.columns[10]] = data_frame[data_frame.columns[1]].apply(
         lambda value: str(value).replace(",", "").replace(".", "").isdigit()
     )
-    data_frame["Formato numero valor 100%"] = data_frame[data_frame.columns[2]].apply(
+    data_frame[historical_df.columns[11]] = data_frame[data_frame.columns[2]].apply(
         lambda value: str(value).replace(",", "").replace(".", "").isdigit()
     )
 
-    data_frame["Fecha propuesta"] = values_validation.get_file_date()
+    data_frame[historical_df.columns[12]] = values_validation.get_file_date()
+    # Set unique columns
+    data_frame.columns = historical_df.columns
+    return data_frame
 
 
 def validate_values(acm_files: list[str]) -> None:
@@ -196,21 +223,121 @@ def validate_values(acm_files: list[str]) -> None:
             values_validation.file_path, values_validation.sheet_name
         )
         # Get previous data frame
-        previous_data_frame: pd.DataFrame = pd.read_excel(
-            values_validation.previous_file,
-            engine="pyxlsb",
-            sheet_name="2024",
-            dtype=str,
+        historical_df: pd.DataFrame = values_validation.read_excel(
+            values_validation.previous_file, values_validation.sheet_name
         )
         # Validate and extract the important data from propuesta and acm report
         propuesta_df: pd.DataFrame = extract_data_from_propuesta(propuesta_pago_df)
         acm_report: pd.DataFrame = get_acm_report(acm_files)
         merged_df: pd.DataFrame = cross_file(propuesta_df, acm_report)
+        # Apply formulas to validate inconsistencies
+        filled_df: pd.DataFrame = apply_formulas(merged_df, historical_df)
+        # Report inconsistencies
+        report_inconsistencies(filled_df)
+        # Concat data frames to save it into a temp file
+        final_df: pd.DataFrame = pd.concat(
+            [historical_df, filled_df], ignore_index=True
+        )
+        # Save the final file
+        final_df.to_excel(
+            values_validation.temp_file,
+            sheet_name=values_validation.sheet_name,
+            index=False,
+        )
+        return True, "Validación de valores realizada correctamente"
 
-        apply_formulas(merged_df, previous_data_frame)
-        return merged_df
     except Exception as e:
         return False, f"Error: {e}"
+
+
+def report_inconsistencies(data_frame: pd.DataFrame) -> None:
+    """Method to generate a report of inconsistencies and save it into tbe correct file"""
+    try:
+        # 1. Valores validation
+        valores_exception_df: pd.DataFrame = values_validation.read_excel(
+            values_validation.exception_file, "VALIDACION VALORES"
+        )
+        valores_exception_list: list[str] = (
+            valores_exception_df.iloc[:, 0].dropna().astype(str).to_list()
+        )
+        valores_inconsistencies: pd.DataFrame = data_frame[~data_frame.iloc[:, 7]]
+        valores_inconsistencies = valores_inconsistencies[
+            ~valores_inconsistencies.iloc[:, 3].isin(valores_exception_list)
+        ]
+        # Save the inconsistencies
+        values_validation.validate_inconsistencies(
+            valores_inconsistencies, [3, 7], "ValidacionValores"
+        )
+        # 2. Radicados number duplicated
+        duplicados_exception_list: pd.DataFrame = values_validation.read_excel(
+            values_validation.exception_file, "VALIDACION DUPLICADOS"
+        )
+        radicados_exception_list: list[str] = (
+            duplicados_exception_list.iloc[:, 0].dropna().astype(str).to_list()
+        )
+        radicados_inconsistencies: pd.DataFrame = data_frame[
+            data_frame.iloc[:, 8].astype(str) == "2"
+        ]
+        radicados_inconsistencies = radicados_inconsistencies[
+            ~radicados_inconsistencies.iloc[:, 1].isin(radicados_exception_list)
+        ]
+        # Save the inconsistencies
+        values_validation.validate_inconsistencies(
+            radicados_inconsistencies, [1, 8], "ValidacionRadicadosDuplicados"
+        )
+        # 3. Key duplicated
+        key_exception_list: list[str] = (
+            duplicados_exception_list.iloc[:, 1].dropna().astype(str).to_list()
+        )
+        key_inconsistencies: pd.DataFrame = data_frame[
+            data_frame.iloc[:, 9].astype(str) == "2"
+        ]
+        key_inconsistencies = key_inconsistencies[
+            ~key_inconsistencies.iloc[:, 3].isin(key_exception_list)
+        ]
+        # Save the inconsistencies
+        values_validation.validate_inconsistencies(
+            key_inconsistencies, [3, 9], "ValidacionKeyDuplicados"
+        )
+        # 4. Radicado format
+        format_exception_df: pd.DataFrame = values_validation.read_excel(
+            values_validation.exception_file, "VALIDACION FORMATOS"
+        )
+        radicados_format_exception_list: list[str] = (
+            format_exception_df.iloc[:, 0].dropna().astype(str).to_list()
+        )
+        radicados_format_inconsistencies: pd.DataFrame = data_frame[
+            ~data_frame.iloc[:, 10].astype(bool)
+        ]
+        radicados_format_inconsistencies = radicados_format_inconsistencies[
+            ~radicados_format_inconsistencies.iloc[:, 3].isin(
+                radicados_format_exception_list
+            )
+        ]
+        # Save the inconsistencies
+        values_validation.validate_inconsistencies(
+            radicados_format_inconsistencies, [3, 10], "ValidacionRadicadoFormato"
+        )
+        # 5. Valor 100% format
+        valor_100_format_exception_list: list[str] = (
+            format_exception_df.iloc[:, 1].dropna().astype(str).to_list()
+        )
+        valor_100_format_inconsistencies: pd.DataFrame = data_frame[
+            ~data_frame.iloc[:, 11].astype(bool)
+        ]
+        valor_100_format_inconsistencies = valor_100_format_inconsistencies[
+            ~valor_100_format_inconsistencies.iloc[:, 2].isin(
+                valor_100_format_exception_list
+            )
+        ]
+        # Save the inconsistencies
+        values_validation.validate_inconsistencies(
+            valor_100_format_inconsistencies, [2, 11], "ValidacionValor100Formato"
+        )
+        return True
+    except Exception as e:
+        print(f"Error: {e}")
+        return False
 
 
 # Call the main function
@@ -225,6 +352,7 @@ def main(params: dict) -> bool:
                 sheet_name=params.get("sheet_name"),
                 file_name=params.get("file_name"),
                 previous_file=params.get("previous_file"),
+                temp_file=params.get("temp_file"),
             )
             return True
     except Exception as e:
@@ -239,9 +367,10 @@ if __name__ == "__main__":
         "exception_file": r"C:\ProgramData\AutomationAnywhere\Bots\AD_GI_BasePagosRedAsistencial_SabanaPagosBasesSiniestralidad\Input\EXCEPCIONES BASE PAGOS RED ASISTENCIAL.xlsx",
         "sheet_name": "Propuesta",
         "file_name": "PROPUESTA DE PAGO (23-10-2024).xlsx",
-        "previous_file": r"C:\ProgramData\AutomationAnywhere\Bots\AD_GI_BasePagosRedAsistencial_SabanaPagosBasesSiniestralidad\Input\VALIDACION PROPUESTA 23-10-2024 PLANTILLA.xlsb",
+        "previous_file": r"C:\ProgramData\AutomationAnywhere\Bots\AD_GI_BasePagosRedAsistencial_SabanaPagosBasesSiniestralidad\Output\Historico Pagos Red Asistencial\RED ASISTENCIAL 23102024.xlsx",
+        "temp_file": r"C:\ProgramData\AutomationAnywhere\Bots\AD_GI_BasePagosRedAsistencial_SabanaPagosBasesSiniestralidad\Temp\Pagos red asistencial 18122024.xlsx",
     }
-    print(main(params))
+    main(params)
     incomes = [
         r"C:\ProgramData\AutomationAnywhere\Bots\AD_GI_BasePagosRedAsistencial_SabanaPagosBasesSiniestralidad\Temp\FCT_RS_REPORTE_WS_AUDITORIA Desde el 01-01-2024 Hasta el 30-06-2024.xlsx",
         r"C:\ProgramData\AutomationAnywhere\Bots\AD_GI_BasePagosRedAsistencial_SabanaPagosBasesSiniestralidad\Temp\FCT_RS_REPORTE_WS_AUDITORIA Desde el 01-07-2024 Hasta el 24-10-2024.xlsx",
